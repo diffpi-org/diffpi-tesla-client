0176cfd1d812cc3d1ec743f95c29a02007b501ce
diffpi-one <one__l@outlook.com>
2016-04-20 17:43:37 +0800

    test1

--- src/test/java/cn/diffpi/security/SecurityTest.java
+++ src/test/java/cn/diffpi/security/SecurityTest.java
@@ -22,7 +22,7 @@ public class SecurityTest {
 		String lol = signatureUtil.digest(xml, "MD5");
 		String signature = signatureUtil.generateSignature(appid, token, lol, millis);
 
-		System.out.println(xml);//ccccbbbb
+		System.out.println(xml);//ddddd
 		System.out.println(signature);
 
 		
